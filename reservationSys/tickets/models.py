4dd9b04fde1870b2e9d4d94c4a561fa22ca20a72
Youssef Nagy <ynagy0424@gmail.com>
2024-09-02 12:45:58 +0300

    add models

--- reservationSys/tickets/models.py
+++ reservationSys/tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.Model):
+    hall = models.CharField(max_length=10)
+    movie = models.CharField(max_length=20)
+
+class Guest(models.Model):
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=15)
+
+
+class Reservation(models.Model):
+    movie = models.ForeignKey(Movie, related_name='reservation', on_delete=models.CASCADE)
+    guest = models.ForeignKey(Guest, related_name='guest', on_delete=models.CASCADE)
